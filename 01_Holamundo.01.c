d9b215259a49762a36018485681f9d67ae0c7b3c
Cris_Ortiz <cristian_toby86@hotmail.com>
2024-06-30 10:56:34 -0500

    first commit

---
+++ 01_Holamundo.01.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() 
+{
+    printf("Hola mundo");
+
+//con dos slash es un comentario corto
+    //indicar que el programaa termino correctamente
+    return 0;
+}
+
+/*esto es un comentario
+para este comentario laro es 
+slash+asterisco
+
+ largo */
